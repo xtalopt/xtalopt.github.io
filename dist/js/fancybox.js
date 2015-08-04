81daed75143801c860f8197a86401f071d1284e0
xtalopt <zfalls@me.com>
2015-08-04 15:04:53 -0400

    A LOT OF UPDATES

---
+++ dist/js/fancybox.js
@@ -0,0 +1,5 @@
+  <script type="text/javascript">
+              $(document).ready(function() {
+                                  $(".fancybox").fancybox();
+                                              });
+  </script>
